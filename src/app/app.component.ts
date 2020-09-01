5aeb89fa94fc8ed5f5346fe56c4c0d90e1bd17cd
Gchapliy <grishachapliy1@gmail.com>
2020-09-01 15:22:57 +0300

    components in details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,30 +11,7 @@ export interface Post {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  posts: Post[] = [
-    {title: 'Want to learn Angular', text: 'I am learning components', id: 1},
-    {title: 'Next block', text: 'directives and pipes', id: 2}
-  ];
+export class AppComponent {
 
-  updatePosts(post: Post): void {
-    this.posts.unshift(post);
-    console.log('Post', post);
-  }
-
-  removePost(id: number): void {
-    console.log('id -> ', id);
-    this.posts = this.posts.filter(p => p.id !== id);
-  }
-
-  ngOnInit(): void {
-    setTimeout(() => {
-      console.log('Timeout');
-      this.posts[0] = {
-        title: 'changed',
-        text: 'changed 2',
-        id: 20
-      };
-    }, 5000);
-  }
+  isVisible = true
 }
