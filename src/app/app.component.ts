219bdde538d1a279dbe4cff98279c7a6e0043609
Hryhorii <grishachapliy1@gmail.com>
2020-09-08 12:22:43 +0300

    create directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,27 +11,7 @@ export interface Post{
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
 
-  posts: Post[] = [
-    {title: 'I want to learn Angular', text: 'I am still learning components', id: 1},
-    {title: 'Next block', text: 'About directives', id: 2}
-  ];
 
-  ngOnInit(): void {
-    setTimeout(() => {
-      this.posts[0] = {
-        title: 'changed',
-        text: 'changed 2',
-        id: 33
-      };
-    }, 5000);
-  }
-  updatePosts(post: Post): void {
-    this.posts.unshift(post);
-  }
-
-  removePost(id: number): void {
-    this.posts = this.posts.filter(p => p.id !== id);
-  }
 }
