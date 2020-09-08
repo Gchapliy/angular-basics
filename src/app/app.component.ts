f3ab9f530086a06a949e57d972250c245ae64a56
Hryhorii <grishachapliy1@gmail.com>
2020-09-08 11:05:46 +0300

    change detection strategy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 export interface Post{
   title: string;
@@ -11,12 +11,22 @@ export interface Post{
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   posts: Post[] = [
     {title: 'I want to learn Angular', text: 'I am still learning components', id: 1},
-    {title: 'Next block', text: 'About directives', id: 2}
+   // {title: 'Next block', text: 'About directives', id: 2}
   ];
 
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.posts[0] = {
+        title: 'changed',
+        text: 'changed 2',
+        id: 33
+      };
+    }, 5000);
+  }
   updatePosts(post: Post): void {
     this.posts.unshift(post);
   }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,24 +1,31 @@
 import {
   AfterContentChecked,
-  AfterContentInit, AfterViewChecked, AfterViewInit,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  ChangeDetectionStrategy,
   Component,
   ContentChild,
   DoCheck,
   ElementRef,
+  EventEmitter,
   Input,
-  OnChanges, OnDestroy,
-  OnInit, Output,
-  SimpleChanges,
-  EventEmitter
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+  SimpleChanges
 } from '@angular/core';
 import {Post} from '../app.component';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+  styleUrls: ['./post.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PostComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+export class PostComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked,
+  AfterViewInit, AfterViewChecked, OnDestroy {
 
   @Input() post: Post;
   @ContentChild('info', {static: true}) infoRef: ElementRef;
