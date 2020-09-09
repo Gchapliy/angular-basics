1864a7a8b16c469c59c7f7f26ffd6a59d1c95982
Hryhorii <grishachapliy1@gmail.com>
2020-09-09 12:19:53 +0300

    list filtering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
 import {Component, OnInit} from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  search = '';
+  searchField = 'title';
 
+  posts: Post[] = [
+    {title: 'Beer', text: 'The best beer in the world'},
+    {title: 'Bread', text: 'The best bread in the world'},
+    {title: 'Javascript', text: 'The worst lang in the world'},
+  ];
 
 }
