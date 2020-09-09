83edfde69a5eaac9e453ea0904714b01cd9100b8
Hryhorii <grishachapliy1@gmail.com>
2020-09-09 14:13:38 +0300

    rxjs observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {interval, Subscription} from 'rxjs';
+import {interval, observable, Observable, Subscription} from 'rxjs';
 import {map, filter, switchMap} from 'rxjs/operators';
 
 @Component({
@@ -11,20 +11,31 @@ export class AppComponent {
   sub: Subscription;
 
   constructor() {
-    const intervalStream$ = interval(1000);
-
-    this.sub = intervalStream$
-      .pipe(
-        filter(value => value % 2 === 0),
-        map((value) => `Mapped value ${value}`),
-        switchMap(() => interval(500))
-      )
-      .subscribe((value) => {
-      console.log(value);
+    const stream$ = new Observable(observer => {
+      setTimeout(() => {
+        observer.next(1);
+      }, 1500);
+      setTimeout(() => {
+        observer.complete();
+      }, 2100);
+      setTimeout(() => {
+        observer.error('Something went wrong');
+      }, 2000);
+      setTimeout(() => {
+        observer.next(2);
+      }, 2500);
     });
+
+    this.sub = stream$
+      .subscribe(
+        value => console.log('Next: ', value),
+        error => console.log('Error: ', error),
+        () => console.log('Complete')
+      );
   }
 
   stop(): void {
     this.sub.unsubscribe();
   }
+
 }
