44b28ab1e653ffee6722eeaab8b4cb5b007f3770
Hryhorii <grishachapliy1@gmail.com>
2020-09-09 14:32:02 +0300

    services. AppCounter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {Subject, Subscription} from 'rxjs';
+import {AppCounterService} from './services/app-counter.service';
 
 @Component({
   selector: 'app-root',
@@ -7,23 +7,8 @@ import {Subject, Subscription} from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  sub: Subscription;
+  constructor(private appCounterService: AppCounterService){
 
-  stream$: Subject<number> = new Subject<number>();
-  counter = 0;
-
-  constructor() {
-    this.sub = this.stream$.subscribe(value => {
-      console.log('Subscribe', value);
-    });
   }
 
-  stop(): void {
-    this.sub.unsubscribe();
-  }
-
-  next(): void {
-    this.counter++;
-    this.stream$.next(this.counter);
-  }
 }
