3f2a2dce12f939119199991c51e1ada974e26603
Hryhorii <grishachapliy1@gmail.com>
2020-09-09 15:43:12 +0300

    forms validation init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser'
+import {NgModule} from '@angular/core'
 
-import { AppComponent } from './app.component';
-import {FormsModule} from '@angular/forms';
-import {AppCounterService} from './services/app-counter.service';
-import { CounterComponent } from './counter/counter.component';
+import {AppComponent} from './app.component'
+import {FormsModule} from '@angular/forms'
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [
+
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
