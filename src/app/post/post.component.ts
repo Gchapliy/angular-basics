9aa84e4d404c52bf5d6d9b8f6f0420784b6ed0e5
Hryhorii <grishachapliy1@gmail.com>
2020-09-11 16:01:55 +0300

    posts handling

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,29 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {Post, PostsService} from '../posts.service';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+  post: Post;
 
+  constructor(
+    private postService: PostsService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.post = this.postService.getById(parseInt(params.id));
+    });
+  }
+
+  loadPost(): void {
+    this.router.navigate(['post', 44]);
+  }
 }
