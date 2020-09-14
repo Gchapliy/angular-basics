e3c6d4ab87c7c0f7159091e45419bdb96e409555
Hryhorii <grishachapliy1@gmail.com>
2020-09-14 12:48:01 +0300

    angular component testing

---
+++ src/app/counter/counter.component.spec.ts
@@ -0,0 +1,17 @@
+import {CounterComponent} from './counter.component';
+
+describe('CounterComponent', () => {
+  let component;
+  beforeEach(() => {
+    component = new CounterComponent();
+  });
+
+  it('should incement counter by 1', () => {
+    component.increment();
+    expect(component.counter).toBe(1);
+  });
+  it('should decrement counter by 1', () => {
+    component.decrement();
+    expect(component.counter).toBe(-1);
+  });
+});
