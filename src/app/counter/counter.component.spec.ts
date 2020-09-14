c91a2e22c8df011c479ff9f1b39516e34dc6af1a
Hryhorii <grishachapliy1@gmail.com>
2020-09-14 15:59:34 +0300

    template and properties testing

--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -1,5 +1,6 @@
 import {CounterComponent} from './counter.component';
 import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
 
 describe('CounterComponent', () => {
   let component: CounterComponent;
@@ -16,5 +17,16 @@ describe('CounterComponent', () => {
 
   it('should should be created', function() {
     expect(component).toBeDefined();
+  });
+
+  it('should render counter property', function() {
+    component.counter = 42;
+
+    fixture.detectChanges();
+
+    let de = fixture.debugElement.query(By.css('.counter'));
+    let el: HTMLElement = de.nativeElement;
+
+    expect(el.textContent).toContain('42');
   });
 });
