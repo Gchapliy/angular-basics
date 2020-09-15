879f3dd9f3454ad4d198c31e5f34aa06d7517e4d
Hryhorii <grishachapliy1@gmail.com>
2020-09-15 08:14:17 +0300

    complex transitions done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,19 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
       state('start', style({background: 'blue'})),
       state('end', style({background: 'red', transform: 'scale(1.2)'})),
       state('special', style({
-        background
-      }))
+        background: 'orange',
+        transform: 'scale(0.5',
+        borderRadius: '50%'
+      })),
       transition('start => end', animate(450)),
       transition('end => start', animate('800ms ease-in-out')),
-
+      transition('special <=> *', [
+        style({background: 'green'}),
+        animate('1s', style({
+          background: 'pink'
+        })),
+        animate(750)
+      ]),
     ])
   ]
 })
