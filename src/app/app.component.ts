7fe19c0608d76ebf56299bd5179a66d0d230ace8
Hryhorii <grishachapliy1@gmail.com>
2020-09-15 08:41:19 +0300

    access to children elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {animate, group, sequence, state, style, transition, trigger} from '@angular/animations';
+import {animate, group, query, sequence, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -18,11 +18,17 @@ import {animate, group, sequence, state, style, transition, trigger} from '@angu
       transition('start => end', animate(450)),
       transition('end => start', animate('800ms ease-in-out')),
       transition('special <=> *', [
-        style({background: 'green'}),
-        animate('1s', style({
-          background: 'pink'
-        })),
-        animate(750)
+        group([
+          query('h4', animate(1500, style({
+            fontSize: '.5rem',
+            color: 'red'
+          }))),
+          style({background: 'green'}),
+          animate('1s', style({
+            background: 'pink'
+          })),
+          animate(750)
+        ])
       ]),
       // void => *
       transition(':enter', [
