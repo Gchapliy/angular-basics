709efad094ab53dec52a75f7d4e5505b667ac98c
Gchapliy <grishachapliy1@gmail.com>
2020-09-15 07:31:53 +0300

    adding transition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {state, style, trigger} from "@angular/animations";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-root',
@@ -9,12 +9,15 @@ import {state, style, trigger} from "@angular/animations";
   animations: [
     trigger('box', [
       state('start', style({background: 'blue'})),
-      state('end', style({background: 'red', transform: 'scale(1.2)'}))
+      state('end', style({background: 'red', transform: 'scale(1.2)'})),
+      transition('start => end', animate(450)),
+      transition('end => start', animate('800ms ease-in-out')),
+
     ])
   ]
 })
 export class AppComponent {
-  boxState = 'end';
+  boxState = 'start';
 
   animate() {
     this.boxState = this.boxState === 'end' ? 'start' : 'end';
